9939c578f660c6d1d87f9cf3e00d293c90e54814
Ade-akanfe <aadeakanfe@gmail.com>
2026-08-01 03:20:21 +0100

    working on logic

--- next.config.mjs
+++ next.config.mjs
@@ -25,7 +25,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     async headers() {
-        return [];
+        return [
+            {
+                source: "/live-training",
+                headers: [
+                    {
+                        key: "Cross-Origin-Opener-Policy",
+                        value: "same-origin",
+                    },
+                    {
+                        key: "Cross-Origin-Embedder-Policy",
+                        value: "require-corp",
+                    },
+                ],
+            },
+        ];
     },
 };
 
